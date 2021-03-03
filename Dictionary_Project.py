79f49d3d9b420cd27ec0be4a47db610036ece724
Jon Paulus <jpaul14@acd.ccac.edu>
2021-03-02 20:52:55 -0500

    Add null result output for zip code

--- Dictionary_Project.py
+++ Dictionary_Project.py
@@ -1,6 +1,6 @@
 import csv
 
-dreader = {}
+result = []
 user_search_zip = input("Please enter the five digit zip code you wish to search.  ")
 zip_length = len(user_search_zip)
 
@@ -17,6 +17,18 @@ while zip_length != 5:
 with open('tri_water.csv') as trifile:
     dreader = csv.DictReader(trifile)
     result = [entry for entry in dreader if entry['ZIP_CODE'] == user_search_zip] 
-    print( "Year Carcinogen   Chemical Name")
-    for entry in result:
-        print (entry['REPORTING_YEAR'],"     ", entry['CARCINOGEN'], "     ", entry['CHEM_NAME'] )
+    
+
+def print_result(result):
+    if not result:
+        print (f"No results located for the zip code: {user_search_zip}.  Congratulations on not being poisioned!")
+    else:
+        print( "Year   Carcinogen   Chemical Name") 
+        for entry in result:
+            print (entry['REPORTING_YEAR'],"     ", entry['CARCINOGEN'], "     ", entry['CHEM_NAME'] )
+
+print_result(result)
+
+#Summary line that gives total instances for distinct chemicals, summary line showing total instances per year.
+#count of carcinogen and non carincogen instances per year
+
