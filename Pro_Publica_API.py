26cc97051822980aa5029c652a2be5fdbfe66bc9
Jon Paulus <jpaul14@acd.ccac.edu>
2021-03-08 19:17:36 -0500

    add API security

--- Pro_Publica_API.py
+++ Pro_Publica_API.py
@@ -1,6 +1,9 @@
 import requests, json
 
-pro_publica_req = requests.get('https://api.propublica.org/congress/v1/senate/votes/recent.json', headers ={"X-API-Key" : ""})
+with open("pro_publica_api.txt") as f:
+    pro_publica_key = f.read()
+
+pro_publica_req = requests.get('https://api.propublica.org/congress/v1/senate/votes/recent.json', headers ={"X-API-Key" : pro_publica_key})
 print(pro_publica_req)
 
 if (int(pro_publica_req.status_code) == 200):
