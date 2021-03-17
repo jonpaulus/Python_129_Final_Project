014a53918bc4d78cff152ba67c082c1df0bcd858
Jon Paulus <jpaul14@acd.ccac.edu>
2021-03-16 20:41:41 -0400

    example code for exception walkthrough

---
+++ Exception_lesson.py
@@ -0,0 +1,24 @@
+total_sales = {"John_Doe" : ['three',4,3,2], "Jane_Doe" : [1,6,3,9], "Joe_Doe" : [5,7,2,"X"], "Jill_Doe" : [5,9,-55, 7]}
+
+
+def yearly_total(list):
+    try:
+        return sum(list)
+    except TypeError:
+        return f"Error, Unable to total {list}"   
+
+if yearly_total < 0:
+    raise Exception("Verify Data.  Total sales should not be negative")
+
+
+John_Doe_total = yearly_total(total_sales["John_Doe"])
+Jane_Doe_total = yearly_total(total_sales["Jane_Doe"])
+Joe_Doe_total = yearly_total(total_sales["Joe_Doe"])
+Jill_Doe_total = yearly_total(total_sales["Jill_Doe"])
+
+
+
+print("John's Total",John_Doe_total)
+print("Jane's Total",Jane_Doe_total)
+print("Joe's Total", Joe_Doe_total)    
+print("Jill's Total", Jill_Doe_total)
